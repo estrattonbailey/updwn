90deb5fd51e94c01128e2e05bdaad0343160d7e8
estrattonbailey <ericstrattonbailey@gmail.com>
2017-05-10 17:36:18 -0400

    Updates

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ document.addEventListener('DOMContentLoaded', e => {
   const view = document.getElementById('view')
 
   const ud = updwn({
-    speed: 100
+    speed: 200
   })
 
   ud.on('up', () => view.innerHTML = 'up')
   ud.on('down', () => view.innerHTML = 'down')
+
+  console.log(`
+import updwn from '../package/index.js'
+
+document.addEventListener('DOMContentLoaded', e => {
+  const view = document.getElementById('view')
+
+  const ud = updwn({
+    speed: 200
+  })
+
+  ud.on('up', () => view.innerHTML = 'up')
+  ud.on('down', () => view.innerHTML = 'down')
+})
+  `)
 })
